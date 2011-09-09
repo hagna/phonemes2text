0f28f7a20a5ce703398073cb209344a72f03be02
Nate <natester@gmail.com>
2011-09-09 16:50:52 -0600

    nice help screen

--- src/pygamesteno.py
+++ src/pygamesteno.py
@@ -10,19 +10,69 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 
 
+#wp51 colors
+blue = (0,6,178)
+gray = (170,170,170)
+red = (167,2,0)
+white = (255,255,255)
+
+
 def helper_text():
     if pygame.font:
-        font = pygame.font.Font(None, 36)
-        text = font.render("steno", 1, (10, 10, 10))
-        textpos = text.get_rect(centerx=background.get_width()/2)
-        background.blit(text, textpos)
+        font = pygame.font.Font(None, 26)
+        consonants = ['net 4', 'tin 3',
+                       'r run 1',
+                       's sin 2',
+                       'd din 5',
+                       'l let 1,4',
+                       'th this 2,3',
+                       'z zen 3,4',
+                       'm met 1,2',
+                       'k kin 2,3,4',
+                       'v van 1,3',
+                       'w win 1,2,3,4',
+                       'p pin 1,2,3',
+                       'f fin 1,5',
+                       'b bin 4,5',
+                       'h hen 2,4',
+                       'ng sing 2,3,4,5',
+                       'sh shin 1,3,4',
+                       'g gift 3,4,5',
+                       'y yes 1,2,3,4,5',
+                       'ch chin 2,5',
+                       'j jam 1,4,5',
+                       'th thin 1,2,4',
+                       'zh azure 1,3,4,5']
+        vowels = ['a about 0',
+                  'is Dennis 0,4',
+                  'o bob 0,2',
+                  'i bit 0,1',
+                  'a bat 0,3',
+                  'e bet 0,2,3,4', 
+                  'ea beat 0,2,3',
+                  'a bake 0,5',
+                  'uh but 0,3,4',
+                  'u lute 0,2,3,4,5',
+                  'i bite 0,4,5',
+                  'oa boat 0,2,4',
+                  'oo book 0,3,4,5',
+                  'i bird 0,2,5',
+                  'ou bout 0,2,3,5',
+                  'yu cute 0,3,5',
+                  'oy boy 0,2,4,5']
+        for k, phonemeset in enumerate([consonants, vowels]):
+            for i, s in enumerate(phonemeset):
+                text = font.render(s, 1, gray)
+                textpos = text.get_rect(left=10+k*200,
+                                        bottom=30+i*20)
+                background.blit(text, textpos)
     else:
         print "sorry no font for you"
 
 try:
     from espeak import espeak 
     synth = espeak.synth
-    espeak.set_parameter(espeak.Parameter.Rate, 1)
+    espeak.set_parameter(espeak.Parameter.Rate, 10)
     espeak.set_parameter(espeak.Parameter.Volume, 100)
     espeak.synth("hello there")
     #print espeak.get_parameter("rate")
@@ -129,7 +179,7 @@ try:
     pygame.mouse.set_visible(0)
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((250, 250, 250))
+    background.fill(blue)
     helper_text()
     screen.blit(background, (0, 0))
     pygame.display.flip()
